41980fa46138aaf50016339b290a7461c96f584f
NTVIN <shanmuganathana@bzz.ch>
2021-07-11 19:25:45 +0200

    Css done

--- checklist.js
+++ checklist.js
@@ -1,17 +1,17 @@
 const input = document.querySelector("#add");
-const  btn = document.querySelector("#btn");
+const  buttonAdd = document.querySelector("#btn");
 const list = document.querySelector("#list");
 var el = document.getElementsByTagName('li');
 
-// this function will allow us to add elements when we click the button
-btn.onclick = function(){
+
+buttonAdd.onclick = function(){
 
     var txt = input.value;
     if(txt ==''){
-        alert('Das Feld darf nicht leer sein!');
+        alert('Das Feld darf nicht leer sein!'); // fehlermeldung, wenn das Feld leer ist
     }else{
-        li = document.createElement('li');
-        li.innerHTML = txt;
+        li = document.createElement('li'); //die Liste wird ergänzt
+        li.innerHTML = txt; // der eingegebene Text wird
         list.insertBefore(li,list.childNodes[0]);
         input.value = '';
     }
